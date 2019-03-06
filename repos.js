17b117a05a577adda5404bc9c05d40521f1a2c00
Emma Fisher <fis17001@byui.edu>
2019-03-06 12:29:13 -0700

    makeing it prettier

--- repos.js
+++ repos.js
@@ -3,6 +3,7 @@ const json2csv = require('json2csv').parse;
 const request = require('request-promise');
 
 
+
 function getUserRepos(uri, repos) {
     return request({
         "method": "GET",
@@ -20,29 +21,31 @@ function getUserRepos(uri, repos) {
             repos.push({
                 name: repo.name,
                 clone_url: repo.clone_url,
-                has_issues: repo.has_issues,
+                open_issues: repo.open_issues,
                 url: repo.html_url
             });
         });
         console.log(repos.length + " repos so far");
 
         if (response.headers.link.split(",").filter(function (link) { return link.match(/rel="next"/) }).length > 0) {
-            console.log("There is more.");
             var next = new RegExp(/<(.*)>/).exec(response.headers.link.split(",").filter(function (link) { return link.match(/rel="next"/) })[0])[1];
             return getUserRepos(next, repos);
         }
-        var fields = ['Name', 'Clone Url', 'Has Issues', 'Url'];
-        var csv = json2csv(repos, fields);
-        fs.writeFile('repos.csv', csv, 'utf8', (err) => {
-            if (err) throw err;
-            console.log('The file has been saved');
-        })
+        writeToCsv(repos);
         return repos;
     });
 }
 
+function writeToCsv(repos) {
+    var fields = ['Name', 'Clone Url', 'Has Issues', 'Url'];
+    var csv = json2csv(repos, fields);
+    fs.writeFile('repos.csv', csv, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('The file has been saved');
+    })
+}
 
-(function () {
-    getUserRepos('https://api.github.com/orgs/byuitechops/repos?per_page=100');
-}());
+
+getUserRepos('https://api.github.com/orgs/byuitechops/repos?per_page=100');
+
 
